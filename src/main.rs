fdeefbd78aae2bc3359f62e198b02260259f835d
Zoey <z@zoey.dev>
2021-08-18 13:49:50 -0700

    Add development server

--- src/main.rs
+++ src/main.rs
@@ -2,9 +2,40 @@ use std::path::Path;
 
 use zoey::Site;
 
+#[cfg(feature = "serve")]
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
+enum Mode {
+	Build,
+	Serve,
+}
+
+#[cfg(feature = "serve")]
+#[tokio::main]
+async fn main() -> anyhow::Result<()> {
+	let site = Site::new(Path::new("site"))?;
+
+	let mut mode = Mode::Build;
+	for arg in std::env::args() {
+		if arg == "serve" {
+			mode = Mode::Serve;
+			break;
+		}
+	}
+
+	match mode {
+		Mode::Build => site.build_once()?,
+		Mode::Serve => site.serve().await?,
+	}
+
+	println!("Build complete!");
+
+	Ok(())
+}
+
+#[cfg(not(feature = "serve"))]
 fn main() -> anyhow::Result<()> {
-	let builder = Site::new(Path::new("site"))?;
-	builder.build_once()?;
+	let site = Site::new(Path::new("site"))?;
+	site.build_once()?;
 
 	println!("Build complete!");
 
