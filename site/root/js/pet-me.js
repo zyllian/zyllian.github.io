da4ae7f9ccbab33c66726ac52fb8b87dd33eeb3b
zyl <z@zyl.gay>
2024-06-25 16:38:35 -0700

    make pets once per page load

--- site/root/js/pet-me.js
+++ site/root/js/pet-me.js
@@ -1,8 +1,6 @@
 (function () {
   "use strict";
 
-  const DEBOUNCE_TIMER = 1500;
-
   const url = document.body.classList.contains("debug")
     ? "http://127.0.0.1:8787/api/pet"
     : "https://cf.zyllian.workers.dev/api/pet";
@@ -18,7 +16,7 @@
 
   petButton.addEventListener("click", async () => {
     petButton.disabled = true;
-    setTimeout(() => (petButton.disabled = false), DEBOUNCE_TIMER);
+    petButton.innerText = "thanks! <3";
     const r = await (await fetch(url, { method: "post" })).json();
     if (r.count) {
       count.innerText = r.count;
